cb1b2cbd664de48cd87498684f80bd4443371b76
unknown <negi60239@gmail.com>
2024-05-03 20:13:41 +0530

    task2

--- src/component/Classcomp.js
+++ src/component/Classcomp.js
@@ -31,7 +31,7 @@ class Counter extends Component{
         return(
             <div>
                 <h1>Counter App</h1>
-                <p>count: {count}</p>
+                <p>Count: {count}</p>
                 <button onClick={this.handleIncrement}>Increment</button>
                 <button onClick={this.handleDecrement}>Decrement</button>
             </div>
